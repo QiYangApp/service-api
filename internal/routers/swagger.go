264285652acfbf08746bc59cc653d911cd5e5e79
QiYangXy <notice@myadream.com>
2024-02-26 18:31:11 +0800

    调整swag目录位置

--- internal/routers/swagger.go
+++ internal/routers/swagger.go
@@ -5,7 +5,7 @@ import (
 	"framework/config"
 	swaggerfiles "github.com/swaggo/files"
 	gs "github.com/swaggo/gin-swagger"
-	docs "service-api/docs" // 千万不要忘了导入把你上一步生成的docs
+	swag "service-api/resources/swag" // 千万不要忘了导入把你上一步生成的docs
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,8 +22,8 @@ func (*SwaggerRouter) Handle(r *gin.RouterGroup) {
 		return
 	}
 
-	docs.SwaggerInfo.Host = config.Client().GetString("addr")
-	docs.SwaggerInfo.Version = config.Client().GetString("version")
+	swag.SwaggerInfo.Host = config.Client().GetString("addr")
+	swag.SwaggerInfo.Version = config.Client().GetString("version")
 
 	r.GET("/swagger/*any", gs.WrapHandler(swaggerfiles.Handler))
 }
