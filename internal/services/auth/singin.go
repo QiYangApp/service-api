9ce39edf5f9d6be812dfe8c5029bf7f11c85db34
QiYangXy <notice@myadream.com>
2024-03-20 18:43:38 +0800

    add user singin

--- internal/services/auth/singin.go
+++ internal/services/auth/singin.go
@@ -1,1 +1,27 @@
+// Copyright 2014 The Gogs Authors. All rights reserved.
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
 package auth
+
+import (
+	"context"
+	"ent/models"
+	"ent/types/auth"
+	"strings"
+)
+
+func UserSingIn(ctx context.Context, username, nickname string) (*models.User, *models.Source, error) {
+	trimmedUsername := strings.TrimSpace(username)
+
+	if trimmedUsername == "" {
+		return nil, nil, auth.ErrUserNotExist{Name: username}
+	}
+
+	sourceType = auth.SMTP
+	if strings.Contains(trimmedUsername, "@") {
+
+	}
+
+	return nil, nil, nil
+}
---
+++ pkg/ent/types/auth/errors.go
@@ -0,0 +1,53 @@
+// Copyright 2021 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package auth
+
+import (
+	util "ent/utils"
+	"errors"
+	"fmt"
+)
+
+// ErrUserAlreadyExist represents a "user already exists" error.
+type ErrUserAlreadyExist struct {
+	Name string
+}
+
+// IsErrUserAlreadyExist checks if an error is a ErrUserAlreadyExists.
+func IsErrUserAlreadyExist(err error) bool {
+	var errUserAlreadyExist ErrUserAlreadyExist
+	ok := errors.As(err, &errUserAlreadyExist)
+	return ok
+}
+
+func (err ErrUserAlreadyExist) Error() string {
+	return fmt.Sprintf("user already exists [name: %s]", err.Name)
+}
+
+// Unwrap unwraps this error as a ErrExist error
+func (err ErrUserAlreadyExist) Unwrap() error {
+	return util.ErrAlreadyExist
+}
+
+// ErrUserNotExist represents a "UserNotExist" kind of error.
+type ErrUserNotExist struct {
+	UID  int64
+	Name string
+}
+
+// IsErrUserNotExist checks if an error is a ErrUserNotExist.
+func IsErrUserNotExist(err error) bool {
+	var errUserNotExist ErrUserNotExist
+	ok := errors.As(err, &errUserNotExist)
+	return ok
+}
+
+func (err ErrUserNotExist) Error() string {
+	return fmt.Sprintf("user does not exist [uid: %d, name: %s]", err.UID, err.Name)
+}
+
+// Unwrap unwraps this error as a ErrNotExist error
+func (err ErrUserNotExist) Unwrap() error {
+	return util.ErrNotExist
+}
